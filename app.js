3ba0d8aca4eaafd1a32ee2154784be844d4af7f6
Charelle Constantino <54915426+yeetyah1030@users.noreply.github.com>
2024-02-17 15:28:07 -0500

    a frame component svg template

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+AFRAME.registerComponent('tap-to-place', {
+    init: function () {
+        var sceneEl = document.querySelector('#scene');
 
+        sceneEl.addEventListener('onefingerstart', function (event) {
+            // Get the position where the user tapped
+            var tapPosition = event.detail.intersection.point;
+
+            // Create an SVG image
+            var svgImage = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+            svgImage.setAttribute('x', tapPosition.x);
+            svgImage.setAttribute('y', tapPosition.y);
+            svgImage.setAttribute('width', '100');
+            svgImage.setAttribute('height', '100');
+            svgImage.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'your-svg-image.svg');
+
+            // Append the SVG image to the scene
+            sceneEl.appendChild(svgImage);
+        });
+    }
+});
